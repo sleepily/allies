1720162a4b2b0d900a55973b1501bfd49bc106a8
lilly <lewilli.ilp@gmail.com>
2018-11-30 13:53:31 +0100

    First merge to master including unity project and updated readme
    * Update .gitignore
    
    * Added test file
    
    * Update README.md
    
    * Delete test file.txt
    
    * Add empty Unity project.
    
    * Further set up sample scene
    
    added gamemanager
    added uimanager and canvas
    added ground
    added movable sphere as a mockup charcter
    
    * Moved Scripts into folder, created Sprite folder
    
    * Make Sprite folder visible by adding test sprite
    
    you can remove the test sprite as soon as there is another file in the folder, otherwise the folder won't show up on github

---
+++ allies/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+  public List<GameObject> playerList = new List<GameObject>();
+  GameObject player;
+  Rigidbody rb;
+  public float speed = 5f;
+  public float jumpForce = 10f;
+  
+	void Start ()
+  {
+    player = playerList[0];
+    rb = player.GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+  {
+    GetInput();
+	}
+
+  void GetInput()
+  {
+    var input = Input.GetAxisRaw("Horizontal");
+    float movement = input * speed * Time.deltaTime;
+    player.transform.position += new Vector3(movement, 0, 0);
+
+    float verticalInput = Input.GetKeyDown(KeyCode.W) ? 1f : 0f;
+    verticalInput = Mathf.Clamp01(verticalInput);
+    Vector3 force = Vector3.up * jumpForce * verticalInput;
+    rb.AddForce(force);
+  }
+}
