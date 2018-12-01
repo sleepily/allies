9a5c5febdf1d064dafefa870c32ad30562fc0ce1
lilly <lilly@sleepily.moe>
2018-12-01 14:07:23 +0100

    Continued scene
    added controllable characters (A/D to move, W to jump)
    added ability to switch characters when pressing R
    added level layout
    added colliders etc

--- allies/Assets/Scripts/PlayerManager.cs
+++ allies/Assets/Scripts/PlayerManager.cs
@@ -4,32 +4,59 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-  public List<GameObject> playerList = new List<GameObject>();
-  GameObject player;
-  Rigidbody rb;
+  public List<GameObject> characters = new List<GameObject>();
+  public int characterIndex = 0;
+  GameObject activeCharacter;
+  List<Rigidbody2D> rbs = new List<Rigidbody2D>();
+  Rigidbody2D activeRigidbody;
   public float speed = 5f;
   public float jumpForce = 10f;
   
 	void Start ()
   {
-    player = playerList[0];
-    rb = player.GetComponent<Rigidbody>();
-	}
-	
-	void Update ()
+    foreach (GameObject c in characters)
+      rbs.Add(c.GetComponent<Rigidbody2D>());
+
+    SetActiveCharacter();
+  }
+
+  void Update ()
   {
     GetInput();
 	}
 
+  void SetActiveCharacter()
+  {
+    activeCharacter = characters[characterIndex];
+    activeRigidbody = rbs[characterIndex];
+  }
+
+  void NextCharacter()
+  {
+    if (characterIndex + 1 >= characters.Count)
+      characterIndex = 0;
+    else
+      characterIndex++;
+
+    SetActiveCharacter();
+  }
+
   void GetInput()
   {
+    if (activeRigidbody == null)
+      return;
+
+    if (Input.GetKeyDown(KeyCode.R))
+      NextCharacter();
+
     var input = Input.GetAxisRaw("Horizontal");
     float movement = input * speed * Time.deltaTime;
-    player.transform.position += new Vector3(movement, 0, 0);
+    Vector3 horizontalForce = new Vector3(movement * speed, 0, 0);
+    activeRigidbody.AddForce(horizontalForce);
 
     float verticalInput = Input.GetKeyDown(KeyCode.W) ? 1f : 0f;
     verticalInput = Mathf.Clamp01(verticalInput);
-    Vector3 force = Vector3.up * jumpForce * verticalInput;
-    rb.AddForce(force);
+    Vector3 verticalForce = Vector3.up * jumpForce * verticalInput;
+    activeRigidbody.AddForce(verticalForce);
   }
 }
