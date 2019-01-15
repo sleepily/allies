0076f752d0a141dd3637593c68a621ca6c87077e
lilly <lilly@sleepily.moe>
2019-01-15 09:59:47 +0100

    Finished splash screen, added placeholder intro

---
+++ allies/Assets/Scripts/Tools/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿namespace Tools
+{
+  public static class ExtensionMethods
+  {
+    public static float Map(float value, float fromSource, float toSource, float fromTarget, float toTarget)
+    {
+      return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
+    }
+
+    public static float Map01(float value, float fromSource, float toSource)
+    {
+      return (value - fromSource) / (toSource - fromSource);
+    }
+  }
+}
--- allies/Assets/Scripts/Tools/SpriteFade.cs
+++ allies/Assets/Scripts/Tools/SpriteFade.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Tools;
 
 public class SpriteFade : MonoBehaviour
 {
   public SpriteRenderer spriteRenderer;
-
+  
+  public float timeBeforeFade = 1f;
   public float fadeInTime = 1f;
   public float holdTime = 1f;
   public float fadeOutTime = 1f;
-  public bool finishedFade = false;
+  public float holdTimeAfter = 1f;
+  public bool isFinished = false;
 
-	void Start ()
+  float timestamp_start;
+  float timestamp_fadeIn;
+  float timestamp_hold1;
+  float timestamp_fadeOut;
+  float timestamp_hold2;
+  float timestamp_finished;
+
+  void Start ()
   {
-		
+    CalculateTimestamps();
 	}
 	
 	void Update ()
@@ -21,8 +31,62 @@ public class SpriteFade : MonoBehaviour
     Fade();
 	}
 
+  void CalculateTimestamps()
+  {
+    timestamp_start = Time.time;
+    timestamp_fadeIn = timestamp_start + timeBeforeFade;
+    timestamp_hold1 = timestamp_fadeIn + fadeInTime;
+    timestamp_fadeOut = timestamp_hold1 + holdTime;
+    timestamp_hold2 = timestamp_fadeOut + fadeOutTime;
+    timestamp_finished = timestamp_hold2 + holdTimeAfter;
+  }
+
   void Fade()
   {
-    float alpha = spriteRenderer.color.a;
+    if (isFinished)
+      return;
+
+    float opacity = CalculateOpacityValue();
+
+    SetSpriteOpacity(opacity);
+  }
+
+  float CalculateOpacityValue()
+  {
+    float time = Time.time;
+    float value = 0f;
+
+    if (time < timestamp_fadeIn)
+      value = 0f;
+
+    if (time > timestamp_fadeIn)
+      value = ExtensionMethods.Map01(time, timestamp_fadeIn, timestamp_hold1);
+
+    if (time > timestamp_hold1)
+      value = 1f;
+
+    if (time > timestamp_fadeOut)
+      value = 1 - ExtensionMethods.Map01(time, timestamp_fadeOut, timestamp_hold2);
+
+    if (time > timestamp_hold2)
+      value = 0f;
+
+    if (time > timestamp_finished)
+      FinishFade();
+
+    return value;
+  }
+
+  void SetSpriteOpacity(float opacity)
+  {
+    Color spriteColor = spriteRenderer.color;
+    spriteColor.a = opacity;
+    spriteRenderer.color = spriteColor;
+  }
+
+  public void FinishFade()
+  {
+    isFinished = true;
+    SetSpriteOpacity(0f);
   }
 }
