3ed1a8d311822648629edcdd946fba5275955e48
lilly <lilly@sleepily.moe>
2019-01-29 13:55:46 +0100

    added elevator sprite changing

--- allies/Assets/Scripts/Interactables/Elevator.cs
+++ allies/Assets/Scripts/Interactables/Elevator.cs
@@ -1,7 +1,32 @@
-﻿public class Elevator : FloatableObject
+﻿using UnityEngine;
+
+public class Elevator : FloatableObject
 {
+  public Sprite spriteActivated;
+  public Sprite spriteDeactivated;
+
+  SpriteRenderer spriteRenderer;
+
+  public override void Init()
+  {
+    base.Init();
+    GetComponents();
+  }
+
+  void GetComponents()
+  {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+  }
+
   public override void Deactivate()
   {
+    spriteRenderer.sprite = spriteDeactivated;
     return;
+  }
+
+  public override void Activate()
+  {
+    base.Activate();
+    spriteRenderer.sprite = spriteActivated;
   }
 }
